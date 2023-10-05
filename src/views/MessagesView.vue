6fdb0842dfd9dab6b3fde369d4faeda7478b5183
santiago <santithevenetvalles@gmail.com>
2023-10-05 11:24:38 +0200

    Messages Page interface done

---
+++ src/views/MessagesView.vue
@@ -0,0 +1,105 @@
+<template>
+  <main class="px-[10%] py-8 bg-gray-100 grid grid-cols-5 gap-10">
+    <div class="col-span-1 bg-white rounded-md border border-gray-200 h-fit">
+      <section class="flex justify-between gap-4 items-center p-4">
+        <img
+          class="rounded-full aspect-square object-cover w-14"
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+          alt=""
+        />
+        <span class="text-xl font-semibold">Santiago</span>
+        <span class="text-base text-center text-gray-700">18 minutes ago</span>
+      </section>
+      <section class="flex justify-between gap-4 items-center p-4">
+        <img
+          class="rounded-full aspect-square object-cover w-14"
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+          alt=""
+        />
+        <span class="text-xl font-semibold">Santiago</span>
+        <span class="text-base text-center text-gray-700">18 minutes ago</span>
+      </section>
+      <section class="flex justify-between gap-4 items-center p-4">
+        <img
+          class="rounded-full aspect-square object-cover w-14"
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+          alt=""
+        />
+        <span class="text-xl font-semibold">Santiago</span>
+        <span class="text-base text-center text-gray-700">18 minutes ago</span>
+      </section>
+    </div>
+
+    <div class="col-span-4">
+        <div
+          class="flex flex-col gap-6 h-fit bg-white rounded-md border border-gray-200 p-4"
+        >
+          <div class="flex self-end max-w-[80%] gap-4">
+            <div>
+              <p
+                class="text-neutral-50 bg-indigo-500 p-4 rounded-b-xl rounded-l-xl"
+              >
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia optio
+                soluta earum.Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Quia optio soluta earum.
+              </p>
+              <span class="text-sm text-gray-500">2 min ago</span>
+            </div>
+            <img
+              class="rounded-full aspect-square object-cover h-14"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+              alt=""
+            />
+          </div>
+          <div class="flex max-w-[80%] gap-4">
+            <img
+              class="rounded-full aspect-square object-cover h-14"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+              alt=""
+            />
+            <div>
+              <p class="text-neutral-900 bg-gray-300 p-4 rounded-b-xl rounded-r-xl">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia optio
+                soluta earum.
+              </p>
+              <span class="text-sm text-gray-500">2 min ago</span>
+            </div>
+          </div>
+          <div class="flex max-w-[80%] gap-4">
+            <img
+              class="rounded-full aspect-square object-cover h-14"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+              alt=""
+            />
+            <div>
+              <p class="text-neutral-900 bg-gray-300 p-4 rounded-b-xl rounded-r-xl">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia optio
+                soluta earum.Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Quia optio soluta earum. Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Quia optio soluta earum.Lorem, ipsum
+                dolor sit amet consectetur adipisicing elit. Quia optio soluta
+                earum.
+              </p>
+              <span class="text-sm text-gray-500">2 min ago</span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md border border-gray-200 p-10 mt-4">
+            <form action="" class="flex flex-col">
+              <textarea
+                class="bg-gray-100 rounded-md p-4"
+                placeholder="Write a post"
+              ></textarea>
+              <div class="flex mt-9 border-t border-gray-100 py-4">
+                <button class="bg-indigo-500 rounded-md py-3 px-6 text-white">
+                  Post
+                </button>
+              </div>
+            </form>
+          </div>
+    </div>
+
+  </main>
+</template>
+
+
